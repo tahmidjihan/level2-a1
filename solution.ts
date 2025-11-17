de5978040121a17025f08b1d81448da39532d26a
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-11-17 19:31:29 +0600

    filter active users

--- solution.ts
+++ solution.ts
@@ -1,10 +1,12 @@
 // * Format value - 1
-function formatValue(value: string | number | boolean) {
+function formatValue(
+  value: string | number | boolean
+): string | number | boolean {
   if (typeof value === 'string') {
     return value.toUpperCase();
   } else if (typeof value === 'number') {
     return value * 10;
-  } else if (typeof value === 'boolean') {
+  } else {
     return !value;
   }
 }
@@ -13,7 +15,7 @@ function formatValue(value: string | number | boolean) {
 // ? Not finished as the requirement
 // TODO I have to add guard for each type of Array
 // TODO I have to finish as the requirement
-function getLength(value: string | any[]) {
+function getLength(value: string | any[]): number {
   return value.length;
 }
 
@@ -27,23 +29,40 @@ class Person {
     this.age = age;
   }
   getDetails() {
-    return `Name: ${this.name}, Age: ${this.age}`;
+    return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
 
 // * Filter by rating - 4
 // ? have to recheck the requirements
-function filterByRating(items: { title: string; rating: number }[]) {
-  const filteredArray = items.sort((a, b) => {
-    return a.rating - b.rating;
-  });
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const filteredArray = items.filter((item) => item.rating >= 4.0);
 
   return filteredArray;
 }
-// const books = [
-//   { title: 'Book A', rating: 4.5 },
-//   { title: 'Book B', rating: 3.2 },
-//   { title: 'Book C', rating: 5.0 },
-// ];
+// ? this should be removed before submission
+const books = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+];
 
 // console.log(filterByRating(books));
+// const person1 = new Person('John Doe', 30);
+// console.log(person1.getDetails());
+
+// * Filter active users - 5
+function filterActiveUsers(
+  user: { id: number; name: string; email: string; isActive: boolean }[]
+) {
+  return user.filter((user) => user.isActive);
+}
+// ? this should be removed before submission
+// const users = [
+//   { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+//   { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+//   { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+// ];
+// console.log(filterActiveUsers(users));
